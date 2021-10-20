04205f0f9a460fd96255cb63b8d02e08520f808d
william-jacob <wjacob2103@gmail.com>
2021-10-20 22:30:32 +0900

    [DELETE]

--- src/apis/request.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const { REACT_APP_ALLOW_CORS, REACT_APP_URL } = process.env;
-
-export const instance = axios.create({
-  baseURL: `${REACT_APP_ALLOW_CORS}/${REACT_APP_URL}`,
-});
