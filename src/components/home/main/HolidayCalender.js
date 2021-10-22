c0b1b9fcbbe6b87691d1771f01d806892b949594
william-jacob <wjacob2103@gmail.com>
2021-10-22 23:27:37 +0900

    [UPDATE] use onSlect

--- src/components/home/main/HolidayCalender.js
+++ src/components/home/main/HolidayCalender.js
@@ -1,23 +1,27 @@
 import React, { useState } from "react";
 import { Calendar } from "antd";
 import "antd/dist/antd.css";
+import styled from "@emotion/styled";
 import moment from "moment";
-import styled from "styled-components";
 
 const CalenderWrapper = styled.div`
   width: 500px;
   height: 500px;
 `;
 
-const HolidayCalender = () => {
-  const [value, setValue] = useState(moment("2021-01-01")); //기본값: 현재 날짜
+const HolidayCalender = ({
+  currentDate,
+  onSelectCalenderDate,
+  calenderDate,
+  onClickDate,
+  flag,
+}) => {
+  const [value, setValue] = useState(moment());
+  const [state, setState] = useState(moment());
 
   const onSelect = () => {
-    //날짜 선택시 이벤트
-    setValue(moment("20160101")); //일수 클릭할때
-    setValue(moment("2016-05")); //년도 바꿀때
-    // 사용자가 클릭한 날짜 넣기
-    //사용자가 날짜 선택시 파란색으로 됨
+    // console.log(flag, currentDate);
+    // setState(flag ? state : currentDate);
   };
 
   const onPanelChange = () => {
@@ -28,9 +32,12 @@ const HolidayCalender = () => {
   return (
     <CalenderWrapper>
       <Calendar
-        value={value}
-        onSelect={onSelect}
-        onPanelChange={onPanelChange}
+        // value={flag ? state : currentDate} //prop이름이 반드시 value여야됨
+        // state={state}
+        // onSelectCalenderDate={() => onSelectCalenderDate()}
+        // onSelect={flag ? onSelect : () => onClickDate()}
+        onSelect={onSelect} //이것만 있으면 되는건가
+        // onPanelChange={onPanelChange}
         fullscreen={false}
       />
     </CalenderWrapper>
